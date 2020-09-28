4f8c8243944269cf3c3818b5849c3349bc74ad0c
Isabella J. Tighe <51792911+izzygitsgreen@users.noreply.github.com>
2020-09-27 19:59:11 -0700

    Uploading react movie project
    files included

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import SearchMovies from "./searchMovies";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+      <SearchMovies/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+serviceWorker.unregister();
